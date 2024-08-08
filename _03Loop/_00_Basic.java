ac37f90bd47402b713c08ffc146372082b8b68fd
Jeong Jiyong <98jiyong@gmail.com>
2024-08-08 12:06:35 +0900

    20240808

---
+++ _03Loop/_00_Basic.java
@@ -0,0 +1,58 @@
+package _03Loop;
+
+public class _00_Basic {
+
+	public static void main(String[] args) {
+		int[] a = {30,40,50,55,65};
+		
+		for(int i=4; i>=0; i--) {
+			System.out.println(a[i]);
+		}
+		System.out.println("------------------------");
+		for(int i=0; i<5; i++) {
+			System.out.println(a[(i+1)%5]);
+		}
+		System.out.println("------------------------");
+		for(int i=0; i<5; i++) {
+			if(a[i] >= 50) {
+				System.out.println(a[i]);
+			}
+		}
+
+		System.out.println("------------------------");
+		int sum = 0;
+		for(int i=0; i<5; i++) {
+			if(a[i] >= 50) {
+				sum += a[i];
+			}
+		}
+		System.out.println("총점 : " + sum);
+		System.out.println("------------------------");
+		int cnt = 0;
+		for(int i=0; i<5; i++) {
+			if(a[i] >= 50) {
+				cnt++;
+			}
+		}
+		System.out.println("50점 이상의 개수 : " + cnt);
+		System.out.println("------------------------");
+		double avg =(double) sum / cnt;
+		System.out.println("평균 : " + avg);
+		System.out.println("------------------------");
+		
+		int smallestNum = 100;
+		int count = 0;
+		int totalScore = 0;
+		for(int i=0; i<5; i++) {
+			if(smallestNum >= a[i]) {
+				smallestNum = a[i];
+			}
+			totalScore += a[i];
+			count ++;
+		}
+		double average = ((double) totalScore - smallestNum) / ((double) count - 1);
+		System.out.println("최저점을 제외한 평균 : " + average);
+		System.out.println("------------------------");
+	}
+
+}
