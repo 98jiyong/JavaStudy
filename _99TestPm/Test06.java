11ba5e01dd2d02d4a8385eb7c18169f0ab495b69
Jeong Jiyong <98jiyong@gmail.com>
2024-08-13 01:01:35 +0900

    20240813_0101_미완성 풀이

--- _99TestPm/Test06.java
+++ _99TestPm/Test06.java
@@ -58,13 +58,27 @@ public class Test06 {
 		// size가 2일경우에 7개이다. 
 		System.out.println("Q4");
 		int[] arr = {1,0,0,0,1,1,1,0,0,0,0,1,1,1,1,0,0,0,1};
+		int cnt = 0;
+		int result = 0;
 		int size=2;
+		for(int i = 0; i < arr.length-1; i++) {
+			if(arr[i] == arr[i+1]) {
+				cnt++;
+				if(cnt / size != 0) {
+					result ++;
+				}
+			}else {
+				cnt = 0;
+			}
+		}
+		System.out.println(result);
+		System.out.println("------------------");
 		
 		// 5. 숫자는  nextInt(), 문자열을 nextLine() 메서드를 사용한다. 
 		// 이때 문자열의 길이는 0보다 크고 10보다 작으며 짝수만 가능하다.
 		// 조건에 만족될때까지 문자 입력을 받는다.
 		// 문자열을 앞으로 읽으나 뒤로 읽으나 같은 경우의 문자를 판별하는 코드를 작성하세요
-//		System.out.println("Q6");
+//		System.out.println("Q5");
 //		Scanner in = new Scanner(System.in);
 //		String a  = in.nextLine();
 //		String fA = "";
@@ -86,34 +100,75 @@ public class Test06 {
 //				}
 //			}
 //		}
+//		System.out.println("------------------");
 		// 6.수식을 계산하는 프로그램을 작성하세요.
 		// 수식 계산 규칙은 연산자 우선순위 없이 앞에서 부터 차례대로 계산한다.
 		// 지금까지 공부한 내용으로 해결하시고, 형변환만 검색이 가능합니다. 
 		// 메서드는 최대한 적게 사용하세요
 		// 20240812 -> +,-,*,/ 값을 따로 체크해서 계산하는 방법으로
+		// 부호를 만났을 때 앞에 값을 더하기
+		// 부호 앞의값을 저장하고 그 앞의 부호를 저장 ex) +면 56을 저장하고 -를 저장해서 계산 
+		// 앞에 부호의 위치를 계산하려면 부호 전의 숫자의 개수를 세는 카운트 변수를 만들고 
+		// ex) -는 인덱스 2번 +는 인덱스 5번 그사이 숫자가 2개니까 카운트를 3증가
+		// if( b.charAt(i-카운트) == '부호') => sum = sum 부호 저장 값
+		
+		
 		System.out.println("Q6");
 		String b ="23-56+45*2-56";
 		String cal = "";
+		String dal = "";
 		int num = 0;
-		int sum = 0;
+		int sum = 23;
 		char sign = ' ';
-		for(int i = 0; i < b.length(); i++) {
+		for(int i = 0; i < b.length()-3; i++) {
 			if((int)b.charAt(i) >= 48) {
 				cal += b.charAt(i);
-				System.out.println(cal);
 			}else {
-				num = Integer.parseInt(cal);
-				System.out.println("num" + num);
-				sum += (int) sign  + num;
-				System.out.println("sign" + sign);
-				System.out.println("sum" + sum);
 				sign = b.charAt(i);
-				cal = "";
-				
+				if(sign == '+') {
+					if((int)b.charAt(i+1) >= 48) {
+						dal += b.charAt(i+1);
+					}
+					if((int)b.charAt(i+2) >= 48) {
+						dal += b.charAt(i+2);
+					}
+					sum += Integer.parseInt(dal);
+					System.out.println(sum);
+					dal = "";
+				}else if(sign == '-') {
+					if((int)b.charAt(i+1) >= 48) {
+						dal += b.charAt(i+1);
+					}
+					if((int)b.charAt(i+2) >= 48) {
+						dal += b.charAt(i+2);
+					}
+					sum -= Integer.parseInt(dal);
+					System.out.println(sum);
+					dal = "";
+				}else if(sign == '*') {
+					if((int)b.charAt(i+1) >= 48) {
+						dal += b.charAt(i+1);
+					}
+					if((int)b.charAt(i+2) >= 48) {
+						dal += b.charAt(i+2);
+					}
+					sum *= Integer.parseInt(dal);
+					System.out.println(sum);
+					sum += Integer.parseInt(cal);	
+					dal = "";
+				}else if(sign == '/') {
+					if((int)b.charAt(i+1) >= 48) {
+						dal += b.charAt(i+1);
+					}
+					if((int)b.charAt(i+2) >= 48) {
+						dal += b.charAt(i+2);
+					}
+					sum /= Integer.parseInt(dal);
+					System.out.println(sum);
+					dal = "";
+				}
 			}
 		}
-		sum = sum + sign  + Integer.parseInt(cal);
-		System.out.println(sum);
 		System.out.println("------------------");
 		
 		// 7.
@@ -138,5 +193,5 @@ public class Test06 {
 		System.out.println("------------------");
 		
 	}
-
 }
+
