e10d8ade41cc3ec05958b9c186607bbf8dda1e3b
Jeong Jiyong <98jiyong@gmail.com>
2024-09-11 17:31:31 +0900

    20240911_twoDimensionalArray_Day2

--- _07twoDimensionalArray/_01_Test.java
+++ _07twoDimensionalArray/_01_Test.java
@@ -6,13 +6,6 @@ public class _01_Test {
 		int[][] a = new int[4][5];
 		int num = 1;
 		
-//		for(int i = 0; i<a[0].length; i++) {
-//			for(int j = 0; j<a.length; j++) {
-//				a[((a.length - 1) - j)][((a[0].length - 1) - i)] = num;
-//				num++;
-//			}
-//		}
-		
 		for(int i=a[0].length-1; i>=0; i--) {
 			for(int j=0; j<a.length; j++) {
 				 a[j][i] = num;
--- _07twoDimensionalArray/_05_Test.java
+++ _07twoDimensionalArray/_05_Test.java
@@ -20,6 +20,7 @@ public class _05_Test {
 		 * 세로 줄은 아래 위 아래 
 		 * 가로줄 -> 세로줄 -> 가로줄 -> 세로줄... 번갈아가면서 진행 
 		 * 돌 떄마다 위아래로 번갈아가면서 진행 (조건 2) -> 짝수 홀수 기준으로 설정하면 될 것 같음
+		 * 줄이 바뀔 때마다 한계씩 줄어들기 때문에 초기값 조건값을 변수로 집어넣어야할 것 같음
 		 */
 		
 		int[][] a = new int[4][5];
@@ -27,22 +28,57 @@ public class _05_Test {
 		int tr = 0;
 		int aRow = a.length;
 		int aCol = a[0].length;
-		int s_r = 0;
-		int e_r = 0;
-		// 세로 줄을 순서대로 구하는 공식
-		for(int i=0; i<aCol; i++) {
-			for(int j=0; j<aRow; j++) {
-				a[j][i] = num;
-				num++;
-			}
-		}
+		int sRowNum = 0;
+		int eRowNum = aRow;
+		int sColNum = 0;
+		int eColNum = aCol;
+//		for(;;) {
+//			break;
+//		}
 		// 가로 줄을 순서대로 구하는 공식
-		for(int i=s_r; i<e_r; i++) {
-			for(int j=0; j<aCol; j++) {
-				a[i][j] = num;
-				num++;
+		for(;num < 20;) {
+			
+			for(int i=sRowNum; i<eRowNum;) {
+				for(int j=sColNum; j<eColNum; j++) {
+					a[i][j] = num;
+					num++;
+				}
+				tr++;
+				break;
 			}
+			
+			// 세로 줄을 순서대로 구하는 공식
+			for(int i=eColNum-1; i>=sColNum;) {
+				for(int j=sRowNum+1; j<eRowNum; j++) {
+					a[j][i] = num;
+					num++;
+				}
+				tr++;
+				break;
+			}
+			
+			// 가로 줄을 거꾸로 구하는 방식
+			for(int i=eRowNum-1; i>=sRowNum;) {
+				for(int j=eColNum-2; j>=sColNum; j--) {
+					a[i][j]=num;
+					num++;
+				}
+				tr++;
+				break;
+			}
+			
+			// 세로 줄을 거꾸로 구하는 방식
+			for(int i=sColNum; i<eColNum;) {
+				for(int j=eRowNum-2; j>=sRowNum+1; j--) {
+					a[j][i]=num;
+					num++;
+				}
+				tr++;
+				break;
+			}
+			
 		}
+		
 		for(int i=0; i<a.length; i++) {
 			for(int j=0; j<a[0].length; j++) {
 				System.out.print(a[i][j] + " ");
