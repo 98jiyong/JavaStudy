60e0575f97a55d7d31e6eea59cbc2cd668b2a3fe
Jeong Jiyong <98jiyong@gmail.com>
2024-09-25 17:45:35 +0900

    20240925

--- _03Loop/_00_Basic.java
+++ _03Loop/_00_Basic.java
@@ -4,7 +4,6 @@ public class _00_Basic {
 
 	public static void main(String[] args) {
 		int[] a = {30,40,50,55,65};
-		
 		for(int i=4; i>=0; i--) {
 			System.out.println(a[i]);
 		}
---
+++ _10Class/Main.java
@@ -0,0 +1,9 @@
+package _10Class;
+
+public class Main {
+
+	public static void main(String[] args) {
+		new MyDogList();
+	}
+
+}
