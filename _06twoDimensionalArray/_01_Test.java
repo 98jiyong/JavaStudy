2c95e4a8728e4405a884c878f26812365e2e8e0d
Jeong Jiyong <98jiyong@gmail.com>
2024-10-09 21:14:51 +0900

    modified

--- _06twoDimensionalArray/_01_Test.java
+++ _06twoDimensionalArray/_01_Test.java
@@ -3,12 +3,14 @@ package _06twoDimensionalArray;
 public class _01_Test {
 
 	public static void main(String[] args) {
+		
 		int[][] a = {
 				{1,2,3,4,5},
 				{6,7,8,9,10},
 				{11,12,13,14,15},
 				{16,17,18,19,20},
 				{21,22,23,24,25}};
+		
 		// a의 2번째 행은 index가 1인 6,7,8,9,10 이기 때문에
 		// 2차원 배열 첫번째 배열 값을 index 1로 설정해 행을 고정하고
 		// 열에대한 값을 반복문을 사용해서 출력
